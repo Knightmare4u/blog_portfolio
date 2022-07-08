7a0b29ba512cb51455ea13afde1b20e062ba2882
Eduardo Bertrand <ebertrand@vb.intra.virbac.com>
2022-07-08 11:12:07 +0800

    Added Post CRUD ability

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,56 @@
+class PostsController < ApplicationController
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
+	def index
+	end
+
+	def new
+		@post = Post.new	
+	end
+
+	def create
+		@post = Post.new post_params
+
+		if @post.save
+			redirect_to @post, notice: "post has been saved"
+		else
+			render 'new', notice: "Post could NOT be saved"
+		end
+	end
+
+	def show
+		
+	end
+
+	def edit
+		
+	end
+
+	def update
+		if @post.update post_params
+			redirect_to @post, notice: "Your article has been updated"
+		else
+			render 'edit'
+		end
+		
+	end
+
+	def destroy
+  @post.destroy
+  redirect_to posts_url, status: :see_other, notice: "Post was successfully destroyed."
+end
+
+	def find_post
+		@post = Post.find_by(id: params[:id])
+		end
+	end
+
+    public
+
+	def  post_params
+		params.require(:post).permit(:title, :content)
+	end
+		
+	
+
+
+
